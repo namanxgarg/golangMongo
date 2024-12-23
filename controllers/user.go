284826f927399473d44eaca81c6fa9ffaaa4e7b9
namanxgarg <social.namangarg@gmail.com>
2024-12-23 13:36:01 +0530

    built the createUser method

--- controllers/user.go
+++ controllers/user.go
@@ -42,3 +42,22 @@ func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httpr
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "%s\n", uj)
 }
+
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
+	u:=models.User{}
+
+	json.NewDecoder(r.Body).Decode(&u)
+
+	u.Id=bson.NewObjectId()
+	uc.session.DB("mongo-golang").C("users").Insert(u)
+
+	uj, err:=json.Marshal(u)
+
+	if err!=nil{
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+}
