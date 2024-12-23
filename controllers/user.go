56aab1bd5abd7520205c659768f673197f78f6b2
namanxgarg <social.namangarg@gmail.com>
2024-12-23 13:57:30 +0530

    updated the code for new mongodb driver

--- controllers/user.go
+++ controllers/user.go
@@ -1,82 +1,113 @@
 package controllers
 
 import (
+	"context"
 	"encoding/json"
+
+	"golangMongo/models"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserController struct {
-	session *mgo.Session
+	client *mongo.Client
 }
 
-func NewUserController(s *mgo.Session) *UserController {
-	return &UserController{s}
+func NewUserController(client *mongo.Client) *UserController {
+	return &UserController{client}
 }
 
+// Helper function to send JSON response
+func sendJSONResponse(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if data != nil {
+		json.NewEncoder(w).Encode(data)
+	}
+}
+
+// GetUser retrieves a user by ID
 func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	id := p.ByName("id")
 
-	if !bson.IsObjectIdHex(id) {
-		w.WriteHeader(http.StatusNotFound)
-	}
-
-	oid := bson.=bson.ObjectIdHex(id)
-
-	u:=models.User{}
-
-	if err:=uc.Session.DB("mongo-golang").C("users").FindId(oid).One(&u); err!=nil{
-		w.WriteHeader(404)
+	// Validate ObjectId
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		sendJSONResponse(w, http.StatusNotFound, map[string]string{"error": "Invalid user ID"})
 		return
 	}
 
-	uj, err:=json.Marshal(u)
-	if err!=nil{
-		fmt.Println(err)
-	}
+	// Connect to the collection
+	collection := uc.client.Database("mongo-golang").Collection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "%s\n", uj)
-}
-
-func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
-	u:=models.User{}
-
-	json.NewDecoder(r.Body).Decode(&u)
-
-	u.Id=bson.NewObjectId()
-	uc.session.DB("mongo-golang").C("users").Insert(u)
-
-	uj, err:=json.Marshal(u)
-
-	if err!=nil{
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "%s\n", uj)
-}
-
-func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params){
-	id:=p.ByName("id")
-
-	if !bson.IsObjectIdHex(id){
-		w.WriteHeader(404)
+	// Find user by ID
+	var user models.User
+	err = collection.FindOne(ctx, bson.M{"_id": objID}).Decode(&user)
+	if err != nil {
+		sendJSONResponse(w, http.StatusNotFound, map[string]string{"error": "User not found"})
 		return
 	}
 
-	oid:=bson.ObjectIdHex()
+	sendJSONResponse(w, http.StatusOK, user)
+}
 
-	err:=uc.session.DB("mongo-golang").C("users").RemoveId(oid); if err!=nil{
-		w.WriteHeader(404)
+// CreateUser creates a new user
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var user models.User
+
+	// Decode the request body into the user struct
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		sendJSONResponse(w, http.StatusBadRequest, map[string]string{"error": "Invalid request payload"})
+		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Deleted user", oid, "\n")
-	
+	// Assign a new ObjectId to the user
+	user.Id = primitive.NewObjectID()
+
+	// Connect to the collection
+	collection := uc.client.Database("mongo-golang").Collection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Insert the user into the database
+	_, err := collection.InsertOne(ctx, user)
+	if err != nil {
+		sendJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": "Failed to create user"})
+		return
+	}
+
+	sendJSONResponse(w, http.StatusCreated, user)
+}
+
+// DeleteUser deletes a user by ID
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	// Validate ObjectId
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		sendJSONResponse(w, http.StatusNotFound, map[string]string{"error": "Invalid user ID"})
+		return
+	}
+
+	// Connect to the collection
+	collection := uc.client.Database("mongo-golang").Collection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Remove the user from the database
+	_, err = collection.DeleteOne(ctx, bson.M{"_id": objID})
+	if err != nil {
+		sendJSONResponse(w, http.StatusNotFound, map[string]string{"error": "User not found"})
+		return
+	}
+
+	sendJSONResponse(w, http.StatusOK, map[string]string{"message": "User deleted successfully"})
 }
--- main.go
+++ main.go
@@ -1,27 +1,48 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"time"
 
 	"golangMongo/controllers"
 
 	"github.com/julienschmidt/httprouter"
-	"gopkg.in/mgo.v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	r := httprouter.New()
-	uc := controllers.NewUserController(getSession())
-	r.GET("/user/{id}", uc.GetUser)
+	client := getMongoClient()
+	uc := controllers.NewUserController(client)
+
+	r.GET("/user/:id", uc.GetUser)
 	r.POST("/user", uc.CreateUser)
-	r.DELETE("/user/{id}", uc.DeleteUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+
+	log.Println("Server running on port 8000")
 	http.ListenAndServe(":8000", r)
 }
 
-func getSession() *mgo.Session {
-	s, err := mgo.Dial("mongodb://localhost:27107")
+func getMongoClient() *mongo.Client {
+	// Create a new MongoDB client
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
 	}
-	return s
+
+	// Test the connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+
+	log.Println("Connected to MongoDB")
+	return client
 }
