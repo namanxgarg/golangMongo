d1a036d4dbda08baf49f0d17aea4bc8d0acebbe3
namanxgarg <social.namangarg@gmail.com>
2024-12-23 13:28:22 +0530

    implemented GET

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module golangMongo
 
 go 1.22.5
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main(){
-    
+import (
+	"net/http"
+
+	"golangMongo/controllers"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/{id}", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/{id}", uc.DeleteUser)
+	http.ListenAndServe(":8000", r)
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:27107")
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
